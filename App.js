330c0800b1cae27da9a3f18aad796adbb22ba45e
Kristiangit <k.numme05@gmail.com>
2023-05-02 20:49:14 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-slate-800">Styling just works! 🎉</Text>
+    </View>
+  );
+};
